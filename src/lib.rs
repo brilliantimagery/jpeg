968997605369367c13ecd7f36f9c10dfadcc002c
chadat23 <chad.derosier@tutanota.com>
2022-03-07 12:22:01 -0800

    Started encoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #![feature(test)]
 
 mod decoder;
+mod encoder;
 
 pub fn decode_image(encoded_image: Vec<u8>) {
     decoder::decode(encoded_image);
+}
+
+pub fn encode_image(raw_image: Vec<u8>, width: usize, height:usize, format: String) {
+    encoder::JPEGEncoder::from_rgb(raw_image, width, height, format);
 }
